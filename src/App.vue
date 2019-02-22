60048723484087b5b9060dee8904221b32b3d469
ninger123 <1106697802@qq.com>
2019-02-22 22:54:49 +0800

    header组件和postlist组件

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+       <PostList></PostList>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from "./components/header"
+  import PostList from "./components/PostList"
+export default {
+  name: 'App',
+  components:{
+    Header,
+    PostList
+  }
+}
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  body{
+    margin: 0;
+    padding: 0;
+  }
+  .main{
+    background-color: rgb(225,225,225);
+  }
+
+</style>
